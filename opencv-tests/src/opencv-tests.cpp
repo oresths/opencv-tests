aba544edf94dcb0d6dd9eb0850ed88b58c286d23
orestis <orestis@ee.auth.gr>
2014-11-21 18:54:34 +0200

    -Count time -same kernels for small sample and sobelx

--- opencv-tests/src/opencv-tests.cpp
+++ opencv-tests/src/opencv-tests.cpp
@@ -20,8 +20,8 @@ using namespace std;
 int main()
 {
 	unsigned char m[5][5] = {{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}, {16,17,18,19,20}, {21,22,23,24,25}};
-	Mat kx = (Mat_<unsigned char>(1,3) << 1, 2, 1);
-	Mat ky = (Mat_<signed char>(1,3) << 1, 0, -1);
+	Mat ky = (Mat_<unsigned char>(1,3) << 1, 2, 1);
+	Mat kx = (Mat_<signed char>(1,3) << 1, 0, -1);
 	Mat dst;
 
 	sepFilter2D(Mat(5, 5, CV_8U, m), dst, CV_16S, kx, ky, Point(-1, -1), 0, BORDER_DEFAULT);
@@ -34,7 +34,14 @@ int main()
     cvtColor(src1, grey, COLOR_BGR2GRAY);
 
     Mat sobelx;
+
+    double exec_time = (double)getTickCount();
+
     Sobel(grey, sobelx, CV_16S, 1, 0);
+
+    exec_time = ((double)getTickCount() - exec_time)*1000./getTickFrequency();
+	cout << "exec_time = " << exec_time << " ms";
+
 
 //    double minVal, maxVal;
 //    minMaxLoc(sobelx, &minVal, &maxVal); //find minimum and maximum intensities
