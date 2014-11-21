8b8e51f0340b0a91b3793beeb684c96d13ea5aa4
orestis <orestis@ee.auth.gr>
2014-11-21 20:22:48 +0200

    Copy sse vectorized code and keep only the interesting kernels

---
+++ neon_filters.cpp
@@ -0,0 +1,225 @@
+#elif CV_NEON
+
+struct SymmRowSmallVec_8u32s
+{
+    SymmRowSmallVec_8u32s() { smallValues = false; }
+    SymmRowSmallVec_8u32s( const Mat& _kernel, int _symmetryType )
+    {
+        kernel = _kernel;
+        symmetryType = _symmetryType;
+        smallValues = true;
+        int k, ksize = kernel.rows + kernel.cols - 1;
+        for( k = 0; k < ksize; k++ )
+        {
+            int v = kernel.ptr<int>()[k];
+            if( v < SHRT_MIN || v > SHRT_MAX )
+            {
+                smallValues = false;
+                break;
+            }
+        }
+    }
+
+    int operator()(const uchar* src, uchar* _dst, int width, int cn) const
+    {
+        if( !checkHardwareSupport(CV_CPU_NEON) )
+            return 0;
+
+        int i = 0, j, k, _ksize = kernel.rows + kernel.cols - 1;
+        int* dst = (int*)_dst;
+        bool symmetrical = (symmetryType & KERNEL_SYMMETRICAL) != 0;
+        const int* kx = kernel.ptr<int>() + _ksize/2;
+        if( !smallValues )
+            return 0;
+
+        src += (_ksize/2)*cn;
+        width *= cn;
+
+        __m128i z = _mm_setzero_si128();
+        if( symmetrical )
+        {
+            if( _ksize == 1 )
+                return 0;
+            if( _ksize == 3 )
+            {
+                if( kx[0] == 2 && kx[1] == 1 )
+                    for( ; i <= width - 16; i += 16, src += 16 )
+                    {
+                        __m128i x0, x1, x2, y0, y1, y2;
+                        x0 = _mm_loadu_si128((__m128i*)(src - cn));
+                        x1 = _mm_loadu_si128((__m128i*)src);
+                        x2 = _mm_loadu_si128((__m128i*)(src + cn));
+                        y0 = _mm_unpackhi_epi8(x0, z);
+                        x0 = _mm_unpacklo_epi8(x0, z);
+                        y1 = _mm_unpackhi_epi8(x1, z);
+                        x1 = _mm_unpacklo_epi8(x1, z);
+                        y2 = _mm_unpackhi_epi8(x2, z);
+                        x2 = _mm_unpacklo_epi8(x2, z);
+                        x0 = _mm_add_epi16(x0, _mm_add_epi16(_mm_add_epi16(x1, x1), x2));
+                        y0 = _mm_add_epi16(y0, _mm_add_epi16(_mm_add_epi16(y1, y1), y2));
+                        _mm_store_si128((__m128i*)(dst + i), _mm_unpacklo_epi16(x0, z));
+                        _mm_store_si128((__m128i*)(dst + i + 4), _mm_unpackhi_epi16(x0, z));
+                        _mm_store_si128((__m128i*)(dst + i + 8), _mm_unpacklo_epi16(y0, z));
+                        _mm_store_si128((__m128i*)(dst + i + 12), _mm_unpackhi_epi16(y0, z));
+                    }
+                else if( kx[0] == -2 && kx[1] == 1 )
+                    return 0;
+                else
+                {
+                    return 0;
+                }
+            }
+            else if( _ksize == 5 )
+            {
+                if( kx[0] == -2 && kx[1] == 0 && kx[2] == 1 )
+                    return 0;
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+        else
+        {
+            if( _ksize == 3 )
+            {
+                if( kx[0] == 0 && kx[1] == 1 )
+                    for( ; i <= width - 16; i += 16, src += 16 )
+                    {
+                        __m128i x0, x1, y0;
+                        x0 = _mm_loadu_si128((__m128i*)(src + cn));
+                        x1 = _mm_loadu_si128((__m128i*)(src - cn));
+                        y0 = _mm_sub_epi16(_mm_unpackhi_epi8(x0, z), _mm_unpackhi_epi8(x1, z));
+                        x0 = _mm_sub_epi16(_mm_unpacklo_epi8(x0, z), _mm_unpacklo_epi8(x1, z));
+                        _mm_store_si128((__m128i*)(dst + i), _mm_srai_epi32(_mm_unpacklo_epi16(x0, x0),16));
+                        _mm_store_si128((__m128i*)(dst + i + 4), _mm_srai_epi32(_mm_unpackhi_epi16(x0, x0),16));
+                        _mm_store_si128((__m128i*)(dst + i + 8), _mm_srai_epi32(_mm_unpacklo_epi16(y0, y0),16));
+                        _mm_store_si128((__m128i*)(dst + i + 12), _mm_srai_epi32(_mm_unpackhi_epi16(y0, y0),16));
+                    }
+                else
+                {
+                    return 0;
+                }
+            }
+            else if( _ksize == 5 )
+            {
+                return 0;
+            }
+        }
+
+        src -= (_ksize/2)*cn;
+        kx -= _ksize/2;
+        for( ; i <= width - 4; i += 4, src += 4 )
+        {
+            __m128i f, s0 = z, x0, x1;
+
+            for( k = j = 0; k < _ksize; k++, j += cn )
+            {
+                f = _mm_cvtsi32_si128(kx[k]);
+                f = _mm_shuffle_epi32(f, 0);
+                f = _mm_packs_epi32(f, f);
+
+                x0 = _mm_cvtsi32_si128(*(const int*)(src + j));
+                x0 = _mm_unpacklo_epi8(x0, z);
+                x1 = _mm_mulhi_epi16(x0, f);
+                x0 = _mm_mullo_epi16(x0, f);
+                s0 = _mm_add_epi32(s0, _mm_unpacklo_epi16(x0, x1));
+            }
+            _mm_store_si128((__m128i*)(dst + i), s0);
+        }
+
+        return i;
+    }
+
+    Mat kernel;
+    int symmetryType;
+    bool smallValues;
+};
+
+
+struct SymmColumnSmallVec_32s16s
+{
+    SymmColumnSmallVec_32s16s() { symmetryType=0; }
+    SymmColumnSmallVec_32s16s(const Mat& _kernel, int _symmetryType, int _bits, double _delta)
+    {
+        symmetryType = _symmetryType;
+        _kernel.convertTo(kernel, CV_32F, 1./(1 << _bits), 0);
+        delta = (float)(_delta/(1 << _bits));
+        CV_Assert( (symmetryType & (KERNEL_SYMMETRICAL | KERNEL_ASYMMETRICAL)) != 0 );
+    }
+
+    int operator()(const uchar** _src, uchar* _dst, int width) const
+    {
+        if( !checkHardwareSupport(CV_CPU_NEON) )
+            return 0;
+
+        int ksize2 = (kernel.rows + kernel.cols - 1)/2;
+        const float* ky = kernel.ptr<float>() + ksize2;
+        int i = 0;
+        bool symmetrical = (symmetryType & KERNEL_SYMMETRICAL) != 0;
+        const int** src = (const int**)_src;
+        const int *S0 = src[-1], *S1 = src[0], *S2 = src[1];
+        short* dst = (short*)_dst;
+        __m128 df4 = _mm_set1_ps(delta);
+        __m128i d4 = _mm_cvtps_epi32(df4);
+
+        if( symmetrical )
+        {
+            if( ky[0] == 2 && ky[1] == 1 )
+            {
+                for( ; i <= width - 8; i += 8 )
+                {
+                    __m128i s0, s1, s2, s3, s4, s5;
+                    s0 = _mm_load_si128((__m128i*)(S0 + i));
+                    s1 = _mm_load_si128((__m128i*)(S0 + i + 4));
+                    s2 = _mm_load_si128((__m128i*)(S1 + i));
+                    s3 = _mm_load_si128((__m128i*)(S1 + i + 4));
+                    s4 = _mm_load_si128((__m128i*)(S2 + i));
+                    s5 = _mm_load_si128((__m128i*)(S2 + i + 4));
+                    s0 = _mm_add_epi32(s0, _mm_add_epi32(s4, _mm_add_epi32(s2, s2)));
+                    s1 = _mm_add_epi32(s1, _mm_add_epi32(s5, _mm_add_epi32(s3, s3)));
+                    s0 = _mm_add_epi32(s0, d4);
+                    s1 = _mm_add_epi32(s1, d4);
+                    _mm_storeu_si128((__m128i*)(dst + i), _mm_packs_epi32(s0, s1));
+                }
+            }
+            else if( ky[0] == -2 && ky[1] == 1 )
+            {
+                return 0;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            if( fabs(ky[1]) == 1 && ky[1] == -ky[-1] )
+            {
+                if( ky[1] < 0 )
+                    std::swap(S0, S2);
+                for( ; i <= width - 8; i += 8 )
+                {
+                    __m128i s0, s1, s2, s3;
+                    s0 = _mm_load_si128((__m128i*)(S2 + i));
+                    s1 = _mm_load_si128((__m128i*)(S2 + i + 4));
+                    s2 = _mm_load_si128((__m128i*)(S0 + i));
+                    s3 = _mm_load_si128((__m128i*)(S0 + i + 4));
+                    s0 = _mm_add_epi32(_mm_sub_epi32(s0, s2), d4);
+                    s1 = _mm_add_epi32(_mm_sub_epi32(s1, s3), d4);
+                    _mm_storeu_si128((__m128i*)(dst + i), _mm_packs_epi32(s0, s1));
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        return i;
+    }
+
+    int symmetryType;
+    float delta;
+    Mat kernel;
+};
