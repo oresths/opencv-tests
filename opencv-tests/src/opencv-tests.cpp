91e92b72cb6ce0b137295a22799bf39230c05a6c
orestis <orestis@ee.auth.gr>
2015-02-28 15:48:31 +0200

    - Create appropriate file names- - Filters test part 1

--- opencv-tests/src/opencv-tests.cpp
+++ opencv-tests/src/opencv-tests.cpp
@@ -7,14 +7,23 @@
 //============================================================================
 
 #include "opencv2/core/core.hpp"
-//#include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/imgcodecs/imgcodecs.hpp"
 #include "opencv2/opencv_modules.hpp"
-//#include "opencv2/opencv.hpp"
+
 #include <iostream>
 #include <fstream>
 #include<errno.h>
+
+#define CANNY 1
+#if !CANNY
+#define FILTERS 1
+#endif
+
+//filename
+#define TYPE "CS" // CS (Canny Steps), CA (Canny All), F (Filters)
+#define CPU "A15" // A7, A9, A15, i7
+#define OPTIMIZ "P" //S (Serial), A (Auto - filters only), P (Parallel)
 
 using namespace cv;
 using namespace std;
@@ -32,31 +41,9 @@ int main(int argc, char **argv) {
     } else
         cout << "Wrong number of arguments" << endl;
 
-//	unsigned char m[5][5] = {{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}, {16,17,18,19,20}, {21,22,23,24,25}};
-
-//	//symmrowsmall8u32s - symmcolumnsmall32s16s, kx<->ky, integer kernel
-	Mat ky = (Mat_<signed char>(1,3) << -1, 0, 1);
-	Mat kx = (Mat_<signed char>(1,3) << 1, 2, 1);
-
-//    Mat ky = (Mat_<signed char>(1, 5) << -5, -2, 0, 2, 5);
-//    Mat kx = (Mat_<signed char>(1, 5) << 1, 3, 10, 3, 1);
-
-//symmrowsmall8u32s if dst=8U
-//	Mat kx = (Mat_<float>(1,5) << 0.0708, 0.2445, 0.3694, 0.2445, 0.0708);
-//	Mat ky = (Mat_<float>(1,5) << 0.0708, 0.2445, 0.3694, 0.2445, 0.0708);
-
-//if (dst==16S && 1<<bits && accept non-integer) symmrowsmall8u32s
-//	Mat kx = (Mat_<float>(1,5) << 0.1, 0.2408, 0.3184, 0.2408, 0.1);
-//	Mat ky = (Mat_<float>(1,5) << -0.9432, -1.1528, 0, 1.1528, 0.9432);
-
-//symmrowsmall8u32s if dst=8U and replace KERNEL_SMOOTH+KERNEL_SYMMETRICAL -> KERNEL_ASYMMETRICAL
-//	Mat kx = (Mat_<float>(1,5) << -0.9432, -1.1528, 0, 1.1528, 0.9432);
-//	Mat ky = (Mat_<float>(1,5) << -0.9432, -1.1528, 0, 1.1528, 0.9432);
-
-//	Mat dst;
-//
-//	sepFilter2D(Mat(5, 5, CV_8U, m), dst, CV_16S, kx, ky, Point(-1, -1), 0, BORDER_DEFAULT);
-
+    uchar num_of_pictures = 3;
+    uchar num_of_tests = 5;
+    double * times = (double *) malloc(loops * num_of_tests * num_of_pictures * sizeof(double));
 
     Mat src4k, grey4k;
     src4k = imread((string(getenv("HOME")) + "/Pictures/people.jpg").c_str(), IMREAD_COLOR);
@@ -74,66 +61,184 @@ int main(int argc, char **argv) {
     src_mess = imread((string(getenv("HOME")) + "/Pictures/xar.jpg").c_str(), IMREAD_COLOR);
     cvtColor(src_mess, grey_mess, COLOR_BGR2GRAY);
 
+    Mat canny_dst;
+
     //preallocate matrix and give values to every element to trigger linux page fault mechanism
     //before filter operations, otherwise page faults during columnfilter would add about 28ms
     //to column filter time
-    double exec_time = (double) getTickCount();
+    Mat filters_dst_4k(grey4k.rows, grey4k.cols, CV_16S, Scalar(0));
+    Mat filters_dst_1080(grey1080.rows, grey1080.cols, CV_16S, Scalar(0));
+    Mat filters_dst_small(grey_small.rows, grey_small.cols, CV_16S, Scalar(0));
+    Mat filters_dst_mess(grey_mess.rows, grey_mess.cols, CV_16S, Scalar(0));
+
+    double exec_time;
     exec_time = (double) getTickCount();
-    Mat sobelx(1080, 1920, CV_8S, Scalar(0));
+    Mat memory_allocations(1080, 1920, CV_16S, Scalar(0)); //pixels , dst type
     exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
     cout << "Memory alloc time = " << exec_time << " ms" << endl;
 
-    ofstream file4k ((std::string(getenv("HOME")) + "/results4k.csv").c_str(), ios::out | ios::trunc);
+    ofstream file4k((std::string(getenv("HOME")) + "/" + TYPE + "4k" + CPU + OPTIMIZ + ".csv").c_str(),
+            ios::out | ios::trunc);
     if (file4k.fail()) {
         cout << "Error: " << DescribeIosFailure(file4k) << endl;
         exit(1);
     }
-    ofstream file1080 ((std::string(getenv("HOME")) + "/results1080.csv").c_str(), ios::out | ios::trunc);
+    ofstream file1080((std::string(getenv("HOME")) + "/" + TYPE + "1080" + CPU + OPTIMIZ + ".csv").c_str(),
+            ios::out | ios::trunc);
     if (file1080.fail()) {
         cout << "Error: " << DescribeIosFailure(file1080) << endl;
         exit(1);
     }
-    ofstream file_small ((std::string(getenv("HOME")) + "/results_small.csv").c_str(), ios::out | ios::trunc);
+    ofstream file_small((std::string(getenv("HOME")) + "/" + TYPE + "s" + CPU + OPTIMIZ + ".csv").c_str(),
+            ios::out | ios::trunc);
     if (file_small.fail()) {
         cout << "Error: " << DescribeIosFailure(file_small) << endl;
         exit(1);
     }
 
-    GaussianBlur(grey4k, grey4k, Size(5,5), 1.1, 0);
-    GaussianBlur(grey1080, grey1080, Size(5,5), 1.1, 0);
-    GaussianBlur(grey_small, grey_small, Size(5,5), 1.1, 0);
-    GaussianBlur(grey_mess, grey_mess, Size(5,5), 1.1, 0);
+    ofstream file_null("/dev/null");
+    if (file_null.fail()) {
+        cout << "Error: " << DescribeIosFailure(file_null) << endl;
+        exit(1);
+    }
 
+
+    Mat canny_src_4k, canny_src_1080, canny_src_small, canny_src_mess;
+    GaussianBlur(grey4k, canny_src_4k, Size(5, 5), 1.1, 0);
+    GaussianBlur(grey1080, canny_src_1080, Size(5, 5), 1.1, 0);
+    GaussianBlur(grey_small, canny_src_small, Size(5, 5), 1.1, 0);
+    GaussianBlur(grey_mess, canny_src_mess, Size(5, 5), 1.1, 0);
+
+    uchar resolut_index, index;
     for (int i = 0; i < loops; ++i) {
 
-//            bilateralFilter(grey, fgrey, 5, 50, 50);
-        //    blur(grey, fgrey, Size(5,5));
+        resolut_index = 0;
 
-//            grey.convertTo(grey, CV_32F);
+        exec_time = (double) getTickCount();
+        Sobel(grey4k, filters_dst_4k, CV_16S, 1, 0);   //x Sobel
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 0;
+        times[index] = exec_time;
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
 
-        //    Sobel(grey, sobelx, CV_16S, 1, 0, -1);//x Scharr
-        //    Sobel(grey, sobelx, CV_16S, 0, 1, -1);//y Scharr kernel_row=[3, 10, 3]
-//        sepFilter2D(grey, sobelx, CV_16S, kx, ky, Point(-1, -1), 0, BORDER_DEFAULT);
-        //    sepFilter2D(grey, sobelx, CV_16S, ky, kx, Point(-1, -1), 0, BORDER_DEFAULT);
+        exec_time = (double) getTickCount();
+        Sobel(grey4k, filters_dst_4k, CV_16S, 0, 1);   //y Sobel
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 1;
+        times[index] = exec_time;
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
 
-//            Canny(grey, sobelx, 100, 150, 3);
+        exec_time = (double) getTickCount();
+        Scharr(grey4k, filters_dst_4k, CV_16S, 1, 0);  //x Scharr
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 2;
+        times[index] = exec_time;
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
 
-//            sobelx.release(); //to reallocate at every round
+        exec_time = (double) getTickCount();
+        Scharr(grey4k, filters_dst_4k, CV_16S, 0, 1);  //y Scharr
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 3;
+        times[index] = exec_time;
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
 
-        Canny(grey4k, sobelx, 100, 150, 3, false, file4k);
+        exec_time = (double) getTickCount();
+        GaussianBlur(grey4k, grey4k, Size(5, 5), 1.1, 0);
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 4;
+        times[index] = exec_time;
+        cvtColor(src4k, grey4k, COLOR_BGR2GRAY); //restores Gaussian src
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
 
-        Canny(grey_mess, sobelx, 100, 150, 3); //causes sobelx reallocation and also messes the caches
+//-----------------------------------------------------------------------------------------------------------
+        resolut_index = 1;
 
-        Canny(grey1080, sobelx, 100, 150, 3, false, file1080);
+        exec_time = (double) getTickCount();
+        Sobel(grey1080, filters_dst_1080, CV_16S, 1, 0);   //x Sobel
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 0;
+        times[index] = exec_time;
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
 
-        Canny(grey_mess, sobelx, 100, 150, 3); //causes sobelx reallocation and also messes the caches
+        exec_time = (double) getTickCount();
+        Sobel(grey1080, filters_dst_1080, CV_16S, 0, 1);   //y Sobel
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 1;
+        times[index] = exec_time;
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
 
-        Canny(grey_small, sobelx, 100, 150, 3, false, file_small);
+        exec_time = (double) getTickCount();
+        Scharr(grey1080, filters_dst_1080, CV_16S, 1, 0);  //x Scharr
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 2;
+        times[index] = exec_time;
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
 
-        Canny(grey_mess, sobelx, 100, 150, 3); //causes sobelx reallocation and also messes the caches
+        exec_time = (double) getTickCount();
+        Scharr(grey1080, filters_dst_1080, CV_16S, 0, 1);  //y Scharr
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 3;
+        times[index] = exec_time;
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
+
+        exec_time = (double) getTickCount();
+        GaussianBlur(grey1080, grey1080, Size(5, 5), 1.1, 0);
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 4;
+        times[index] = exec_time;
+        cvtColor(src1080, grey1080, COLOR_BGR2GRAY); //restores Gaussian src
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
+
+//-----------------------------------------------------------------------------------------------------------
+        resolut_index = 2;
+
+        exec_time = (double) getTickCount();
+        Sobel(grey_small, filters_dst_small, CV_16S, 1, 0);   //x Sobel
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 0;
+        times[index] = exec_time;
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
+
+        exec_time = (double) getTickCount();
+        Sobel(grey_small, filters_dst_small, CV_16S, 0, 1);   //y Sobel
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 1;
+        times[index] = exec_time;
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
+
+        exec_time = (double) getTickCount();
+        Scharr(grey_small, filters_dst_small, CV_16S, 1, 0);  //x Scharr
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 2;
+        times[index] = exec_time;
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
+
+        exec_time = (double) getTickCount();
+        Scharr(grey_small, filters_dst_small, CV_16S, 0, 1);  //y Scharr
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 3;
+        times[index] = exec_time;
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
+
+        exec_time = (double) getTickCount();
+        GaussianBlur(grey_small, grey_small, Size(5, 5), 1.1, 0);
+        exec_time = ((double) getTickCount() - exec_time) * 1000. / getTickFrequency();
+        index = i * num_of_tests * num_of_pictures + resolut_index * num_of_tests + 4;
+        times[index] = exec_time;
+        cvtColor(src_small, grey_small, COLOR_BGR2GRAY); //restores Gaussian src
+        Canny(grey_mess, filters_dst_mess, 100, 150, 3, false, file_null); //messes the caches
+
+#if CANNY
+        Canny(canny_src_4k, canny_dst, 100, 150, 3, false, file4k);
+        Canny(canny_src_mess, canny_dst, 100, 150, 3, false, file_null); //causes canny_dst reallocation and also messes the caches
+
+        Canny(canny_src_1080, canny_dst, 100, 150, 3, false, file1080);
+        Canny(canny_src_mess, canny_dst, 100, 150, 3, false, file_null); //causes canny_dst reallocation and also messes the caches
+
+        Canny(canny_src_small, canny_dst, 100, 150, 3, false, file_small);
+        Canny(canny_src_mess, canny_dst, 100, 150, 3, false, file_null); //causes canny_dst reallocation and also messes the caches
+#endif
     }
-//	exec_time = ((double)getTickCount() - exec_time)*1000./getTickFrequency()/loops;
-//	cout << "average exec_time = " << exec_time << " ms" << endl;
 
     file4k.close();
     file1080.close();
@@ -142,32 +247,30 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-std::string DescribeIosFailure(const std::ios& stream)
-{
-  std::string result;
+std::string DescribeIosFailure(const std::ios& stream) {
+    std::string result;
 
-  if (stream.eof()) {
-    result = "Unexpected end of file.";
-  }
-
+    if (stream.eof()) {
+        result = "Unexpected end of file.";
+    }
 #ifdef WIN32
-  // GetLastError() gives more details than errno.
-  else if (GetLastError() != 0) {
-    result = FormatSystemMessage(GetLastError());
-  }
+    // GetLastError() gives more details than errno.
+    else if (GetLastError() != 0) {
+        result = FormatSystemMessage(GetLastError());
+    }
 #endif
 
-  else if (errno) {
+    else if (errno) {
 #if defined(__unix__)
-    // We use strerror_r because it's threadsafe.
-    // GNU's strerror_r returns a string and may ignore buffer completely.
-    char buffer[255];
-    result = std::string(strerror_r(errno, buffer, sizeof(buffer)));
+        // We use strerror_r because it's threadsafe.
+        // GNU's strerror_r returns a string and may ignore buffer completely.
+        char buffer[255];
+        result = std::string(strerror_r(errno, buffer, sizeof(buffer)));
 #else
-    result = std::string(strerror(errno));
+        result = std::string(strerror(errno));
 #endif
 
-  }
+    }
 
-  return result;
+    return result;
 }
